b37874a1700ad90df2efbae13d4dc34a16b93be1
vasu gupta <vasugupta153@gmail.com>
2024-06-03 03:14:07 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // default output directory
+  },
+  server: {
+    historyApiFallback: true, // This is more useful for local development
+  },
 })
